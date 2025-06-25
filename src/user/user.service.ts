3f3cf458fc8bf0abcd7032a60aa0675858c418de
sxcredlxrd <kirprox13@mail.ru>
2025-06-25 11:18:18 +0600

    feat: implement user creation and retrieval methods

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,37 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
+import { hash } from 'argon2';
+import { AuthDto } from '../auth/dto/auth.dto';
 
 @Injectable()
 export class UserService {
   constructor(private readonly prisma: PrismaService) {}
+
+  async getById(id: string) {
+    const user = await this.prisma.user.findUnique({
+      where: { id },
+      include: { collections: true, sharedLinks: true }
+    });
+
+    return user;
+  }
+
+  async getByEmail(email: string) {
+    const user = await this.prisma.user.findUnique({
+      where: { email },
+      include: { collections: true, sharedLinks: true }
+    });
+
+    return user;
+  }
+
+  async createUser(dto: AuthDto) {
+    return this.prisma.user.create({
+      data: {
+        username: dto.username,
+        email: dto.email,
+        password: await hash(dto.password)
+      }
+    });
+  }
 }
