1237ce4fcc02dd42bd18f74386c526b53ac8639e
sxcredlxrd <kirprox13@mail.ru>
2025-06-25 12:39:24 +0600

    feat: add JWT strategy for validating user tokens

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UserService } from '../../user/user.service';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private readonly userService: UserService,
+    private readonly configService: ConfigService
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: true,
+      secretOrKey: configService.get<string>('JWT_SECRET') as string
+    });
+  }
+
+  async validate({ id }: { id: string }) {
+    return this.userService.getById(id);
+  }
+}
