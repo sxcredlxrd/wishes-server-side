0fcffc2ca29902e3a9fd7d13d6c7a0fa7ce041a2
sxcredlxrd <kirprox13@mail.ru>
2025-06-25 10:38:12 +0600

    fix: update jwtConfig

---
+++ src/config/jwt.config.ts
@@ -0,0 +1,6 @@
+import { ConfigService } from '@nestjs/config';
+import { JwtModuleOptions } from '@nestjs/jwt';
+
+export const getJwtConfig = (configService: ConfigService): JwtModuleOptions => ({
+  secret: configService.get('JWT_SECRET')
+});
