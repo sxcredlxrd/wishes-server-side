5d58bcf8c7a9cb7e1985f41c8415a20131b602cd
sxcredlxrd <kirprox13@mail.ru>
2025-06-25 13:28:30 +0600

    feat: configure JwtModule and register auth strategies in AuthModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UserModule } from '../user/user.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { getJwtConfig } from '../config/jwt.config';
+import { PrismaService } from '../prisma.service';
+import { UserService } from '../user/user.service';
+import { YandexStrategy } from './strategies/yandex.strategy';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
+  imports: [
+    UserModule,
+    ConfigModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getJwtConfig
+    })
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, PrismaService, UserService, JwtStrategy, YandexStrategy]
 })
 export class AuthModule {}
