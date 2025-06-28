67f7e88387a13e214a4b41efaa158ba6eb55c42d
sxcredlxrd <kirprox13@mail.ru>
2025-06-28 19:03:23 +0600

    feat(wish): implement core CRUD operations in WishService

--- src/wish/wish.service.ts
+++ src/wish/wish.service.ts
@@ -1,4 +1,47 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
+import { WishDto } from './dto/wish.dto';
+import { UpdateWishDto } from './dto/update-wish.dto';
 
 @Injectable()
-export class WishService {}
+export class WishService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async createWish(collectionId: string, dto: WishDto) {
+    return this.prisma.wish.create({
+      data: {
+        ...dto,
+        collectionId
+      }
+    });
+  }
+
+  async findAllByCollection(collectionId: string) {
+    return this.prisma.wish.findMany({
+      where: {
+        collectionId: collectionId
+      }
+    });
+  }
+
+  async findOne(id: string) {
+    return this.prisma.wish.findUnique({
+      where: {
+        id
+      }
+    });
+  }
+
+  async updateWish(id: string, dto: UpdateWishDto) {
+    return this.prisma.wish.update({
+      where: { id },
+      data: dto
+    });
+  }
+
+  async deleteWish(id: string) {
+    return this.prisma.wish.delete({
+      where: { id }
+    });
+  }
+}
