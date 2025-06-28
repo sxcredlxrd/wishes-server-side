0f43e50ee1824cade9d4167650dc1c4343fe567c
sxcredlxrd <kirprox13@mail.ru>
2025-06-28 19:16:32 +0600

    chore(app): add CollectionModule and WishModule to AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { CollectionModule } from './collection/collection.module';
+import { WishModule } from './wish/wish.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), AuthModule, UserModule]
+  imports: [ConfigModule.forRoot(), AuthModule, UserModule, CollectionModule, WishModule]
 })
 export class AppModule {}
