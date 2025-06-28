53fa6c51df3bc04396f82289934665ef92ea8a2e
sxcredlxrd <kirprox13@mail.ru>
2025-06-28 21:42:12 +0600

    chore(app): register SharedLinkModule in AppModule to enable shared link functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,9 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { CollectionModule } from './collection/collection.module';
 import { WishModule } from './wish/wish.module';
+import { SharedLinkModule } from './shared-link/shared-link.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), AuthModule, UserModule, CollectionModule, WishModule]
+  imports: [ConfigModule.forRoot(), AuthModule, UserModule, CollectionModule, WishModule, SharedLinkModule]
 })
 export class AppModule {}
