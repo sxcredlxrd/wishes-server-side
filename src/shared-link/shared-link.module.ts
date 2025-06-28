c17cf9e63323e98530005215945d5f7fb4d00adc
sxcredlxrd <kirprox13@mail.ru>
2025-06-28 20:49:53 +0600

    chore(shared-link): register PrismaService as a provider in SharedLinkModule

--- src/shared-link/shared-link.module.ts
+++ src/shared-link/shared-link.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SharedLinkService } from './shared-link.service';
 import { SharedLinkController } from './shared-link.controller';
+import { PrismaService } from '../prisma.service';
 
 @Module({
   controllers: [SharedLinkController],
-  providers: [SharedLinkService],
+  providers: [SharedLinkService, PrismaService]
 })
 export class SharedLinkModule {}
